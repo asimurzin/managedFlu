9f9231de64beeecd849ec71e63de310e5dc2a1c2
Andrey Simurzin <asimurzin@gmail.com>
2011-07-28 18:29:24 +0400

    to follow new paradigm

---
+++ lnInclude/TimeHolder.C
@@ -0,0 +1,28 @@
+#ifndef TimeHolder_C
+#define TimeHolder_C
+
+
+//---------------------------------------------------------------------------
+#include "TimeHolder.H"
+#include "fluTime.H"
+
+
+//---------------------------------------------------------------------------
+Foam::TimeHolder::TimeHolder( const word &name, 
+                              const argList &args, 
+                              const word &systemName, 
+                              const word &constantName ) : 
+                          boost::shared_ptr< Time >( new fluTime( name, args, systemName, constantName ) )
+                          , objectRegistryHolder( boost::shared_ptr< Time >( *this ) )
+{}
+
+
+Foam::TimeHolder::TimeHolder( const boost::shared_ptr< Time >&  t ) : boost::shared_ptr< Time >( t )
+                                                                      , objectRegistryHolder( t ) 
+{} 
+
+Foam::TimeHolder::TimeHolder()
+{}
+
+//---------------------------------------------------------------------------
+#endif
