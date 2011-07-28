c4505ed25e2635a8a979a1348383821c469cb88b
Andrey Simurzin <asimurzin@gmail.com>
2011-07-28 18:18:47 +0400

    to follow new paradigm

---
+++ lnInclude/IOobjectArgs.C
@@ -0,0 +1,18 @@
+#ifndef IOobjectArgs_C
+#define IOobjectArgs_C
+
+
+//---------------------------------------------------------------------------
+#include "IOobjectArgs.H"
+
+
+//---------------------------------------------------------------------------
+Foam::IOobjectArgs::IOobjectArgs( const objectRegistryHolder& orh ) : objectRegistryHolder( orh )
+{}
+
+Foam::IOobjectArgs::IOobjectArgs()
+{}
+
+
+//---------------------------------------------------------------------------
+#endif
