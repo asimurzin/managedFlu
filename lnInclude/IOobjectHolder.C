35c1cca527b459a757c2326b8add184af127a63d
Andrey Simurzin <asimurzin@gmail.com>
2011-07-28 18:21:52 +0400

    to follow new paradigm

---
+++ lnInclude/IOobjectHolder.C
@@ -0,0 +1,26 @@
+#ifndef IOobjectHolder_C
+#define IOobjectHolder_C
+
+
+//---------------------------------------------------------------------------
+#include "IOobjectHolder.H"
+#include "objectRegistryHolder.H"
+#include "fluIOobject.H"
+
+
+//---------------------------------------------------------------------------
+Foam::IOobjectHolder::IOobjectHolder( const word &name, 
+                                      const fileName &instance, 
+                                      const objectRegistryHolder & registry, 
+                                      IOobject::readOption r, 
+                                      IOobject::writeOption w, 
+                                      bool registerObject )
+                           : boost::shared_ptr< IOobject >( new fluIOobject( name, instance, registry, r, w, registerObject ) )
+{}
+
+Foam::IOobjectHolder::IOobjectHolder() : boost::shared_ptr< IOobject >()
+{}
+
+
+//---------------------------------------------------------------------------
+#endif
