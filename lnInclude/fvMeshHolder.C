7e6200bca7ab35ce28eed7a468814bcad6875484
Andrey Simurzin <asimurzin@gmail.com>
2011-07-29 11:58:14 +0400

    follow new paradigm

---
+++ lnInclude/fvMeshHolder.C
@@ -0,0 +1,25 @@
+#ifndef fvMeshHolder_C
+#define fvMeshHolder_C
+
+
+//---------------------------------------------------------------------------
+#include "fvMeshHolder.H"
+#include "flufvMesh.H"
+
+
+//---------------------------------------------------------------------------
+Foam::fvMeshHolder::fvMeshHolder( const IOobjectHolder& io ) : boost::shared_ptr< fvMesh >( new flufvMesh( io ) ),
+                                                               objectRegistryHolder( boost::shared_ptr< fvMesh >( *this ) )
+{}
+
+Foam::fvMeshHolder::fvMeshHolder( const boost::shared_ptr< fvMesh >&  t ) : boost::shared_ptr< fvMesh >( t ),
+                                                                            objectRegistryHolder( t )
+{} 
+
+Foam::fvMeshHolder::fvMeshHolder() : boost::shared_ptr< fvMesh >(),
+                                     objectRegistryHolder()
+{} 
+
+
+//---------------------------------------------------------------------------
+#endif
