d0d6a2e43fec4223702a3c9316d4eeb52a514db5
Andrey Simurzin <asimurzin@gmail.com>
2011-07-29 18:15:22 +0400

    to add dictionaryHolder

---
+++ lnInclude/dictionaryArgs.C
@@ -0,0 +1,18 @@
+#ifndef dictionaryArgs_C
+#define dictionaryArgs_C
+
+
+//---------------------------------------------------------------------------
+#include "dictionaryArgs.H"
+
+
+//---------------------------------------------------------------------------
+Foam::dictionaryArgs::dictionaryArgs()
+{}
+
+Foam::dictionaryArgs::dictionaryArgs( const dictionaryHolder& dh ): dictionaryHolder( dh)
+{}
+
+
+//---------------------------------------------------------------------------
+#endif
