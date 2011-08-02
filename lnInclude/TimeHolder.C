ce21a740d40a12d62dfe8dd1c31db1346dbb5dec
Andrey Simurzin <asimurzin@gmail.com>
2011-08-02 14:17:07 +0400

    to add cout to constructors and destructors

--- lnInclude/TimeHolder.C
+++ lnInclude/TimeHolder.C
@@ -8,19 +8,35 @@
 
 
 //---------------------------------------------------------------------------
-Foam::TimeHolder::TimeHolder( const word &name, 
+namespace Foam
+{
+
+  TimeHolder::TimeHolder( const word &name, 
                               const argList &args, 
                               const word &systemName, 
                               const word &constantName ) : 
-  boost::shared_ptr< Time >( new Time( name, args, systemName, constantName ) )
-  , objectRegistryHolder( boost::shared_ptr< Time >( *this ) )
-{}
+    boost::shared_ptr< Time >( new Time( name, args, systemName, constantName ) )
+    , objectRegistryHolder( boost::shared_ptr< Time >( *this ) )
+  {
+    cout << "TimeHolder = " << this << nl;
+  }
 
 
-Foam::TimeHolder::TimeHolder()
-  : boost::shared_ptr< Time >()
-  , objectRegistryHolder() 
-{}
+  TimeHolder::TimeHolder()
+    : boost::shared_ptr< Time >()
+    , objectRegistryHolder() 
+  {
+    cout << "TimeHolder = " << this << nl;
+  }
+
+  TimeHolder::~TimeHolder()
+  {
+    cout << "~TimeHolder = " << this << nl;
+  }
+
+
+} //Foam
+
 
 //---------------------------------------------------------------------------
 #endif
