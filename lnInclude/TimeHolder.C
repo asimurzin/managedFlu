d70425a2f1702f70bd157ba2f07c243e77c0ad1b
Andrey Simurzin <asimurzin@gmail.com>
2011-08-02 13:29:22 +0400

    remove fluObject

--- lnInclude/TimeHolder.C
+++ lnInclude/TimeHolder.C
@@ -12,15 +12,10 @@ Foam::TimeHolder::TimeHolder( const word &name,
                               const argList &args, 
                               const word &systemName, 
                               const word &constantName ) : 
-  boost::shared_ptr< Time >( new fluTime( name, args, systemName, constantName ) )
+  boost::shared_ptr< Time >( new Time( name, args, systemName, constantName ) )
   , objectRegistryHolder( boost::shared_ptr< Time >( *this ) )
 {}
 
-
-Foam::TimeHolder::TimeHolder( const boost::shared_ptr< Time >&  t )
-  : boost::shared_ptr< Time >( t )
-  , objectRegistryHolder( t ) 
-{} 
 
 Foam::TimeHolder::TimeHolder()
   : boost::shared_ptr< Time >()
