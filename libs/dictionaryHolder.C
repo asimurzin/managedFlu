87e75544f5decd12d162dd7d5e763136c0fcd184
Andrey Simurzin <asimurzin@gmail.com>
2011-08-04 15:32:01 +0400

    minor changes

--- libs/dictionaryHolder.C
+++ libs/dictionaryHolder.C
@@ -12,9 +12,11 @@ namespace Foam
   dictionaryHolder::dictionaryHolder( const boost::shared_ptr< dictionary >& dict ): boost::shared_ptr< dictionary >( dict )
   {}
 
-  dictionaryHolder::dictionaryHolder( ): boost::shared_ptr< dictionary >( new dictionary() )
+  dictionaryHolder::dictionaryHolder(): boost::shared_ptr< dictionary >( new dictionary() )
   {}
-}
+
+}//Foam
+
 
 //---------------------------------------------------------------------------
 #endif
