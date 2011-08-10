7b511db417a3636609e888ea91338e2557a6c442
Andrey Simurzin <asimurzin@gmail.com>
2011-08-10 17:54:36 +0400

    template fvMatrixArgs

---
+++ libs/fvMatrixArgs.C
@@ -0,0 +1,17 @@
+//---------------------------------------------------------------------------
+namespace Foam
+{
+  template< class Type >
+  fvMatrixArgs< Type >::fvMatrixArgs( const GeometricFieldHolder< Type, fvPatchField, volMesh >& field ) 
+    : psi_( new GeometricFieldHolder< Type, fvPatchField, volMesh >( field) )
+  {}
+
+  template< class Type >
+  GeometricFieldHolder< Type, fvPatchField, volMesh >& fvMatrixArgs< Type >::psi() const
+  {
+    return *psi_;
+  }
+}
+
+
+//---------------------------------------------------------------------------
