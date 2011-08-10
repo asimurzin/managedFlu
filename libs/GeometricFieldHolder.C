33c0c3f95d0a63cbcb0423b110ed0232271b1013
Andrey Simurzin <asimurzin@gmail.com>
2011-08-10 10:15:52 +0400

    new GeometricFieldHolder hierarhy

---
+++ libs/GeometricFieldHolder.C
@@ -0,0 +1,75 @@
+//---------------------------------------------------------------------------
+#include "GeometricFieldHolder.H"
+#include "IOobjectHolder.H"
+#include "fvMeshHolder.H"
+
+
+//---------------------------------------------------------------------------
+namespace Foam
+{
+
+  template<class Type, template<class> class PatchField, class GeoMesh>
+  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder( const IOobjectHolder& io, const fvMeshHolder& mesh ) :
+    GeometricFieldArgs( mesh ),
+    tmp< GeometricField< Type, PatchField, GeoMesh > >( new GeometricField< Type, PatchField, GeoMesh >( *io, *mesh ) )
+  {
+#ifdef OUR_DEBUG
+    cout << "GeometricFieldHolder=" << this << nl;
+#endif
+  }
+
+  template<class Type, template<class> class PatchField, class GeoMesh>
+  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder( const tmp< GeometricField< Type, PatchField, GeoMesh > >& tmp_gf, const fvMeshHolder& mesh ) : 
+    GeometricFieldArgs( mesh ), 
+    tmp< GeometricField< Type, PatchField, GeoMesh > >( tmp_gf )
+  {
+#ifdef OUR_DEBUG
+    cout << "GeometricFieldHolder=" << this << nl;
+#endif
+  }
+
+  template<class Type, template<class> class PatchField, class GeoMesh>
+  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder( const IOobjectHolder& io, const GeometricFieldHolder< Type, PatchField, GeoMesh >& field ):
+    GeometricFieldArgs( field.mesh() ),
+    tmp< GeometricField< Type, PatchField, GeoMesh > >( new GeometricField< Type, PatchField, GeoMesh >( *io, field() ) )
+  {
+#ifdef OUR_DEBUG
+    cout << "flusurfaceScalarField = " << this << nl;
+#endif
+  }
+
+  template<class Type, template<class> class PatchField, class GeoMesh>
+  GeometricFieldHolder< Type, PatchField, GeoMesh >::~GeometricFieldHolder()
+  {
+#ifdef OUR_DEBUG
+    cout << "GeometricFieldHolder=" << this << nl;
+#endif
+  }
+  
+  
+  //-------------------------------------------------------------------------
+  template<class Type, template<class> class PatchField, class GeoMesh>
+  GeometricField< Type, PatchField, GeoMesh >& 
+    GeometricFieldHolder< Type, PatchField, GeoMesh >::ref()
+  {
+    return this->operator()();
+  }
+  
+  template<class Type, template<class> class PatchField, class GeoMesh>
+  void GeometricFieldHolder< Type, PatchField, GeoMesh >::operator = ( const GeometricFieldHolder< Type, PatchField, GeoMesh >& field)
+  {
+    this->ref() = field();
+  }
+  
+  template<class Type, template<class> class PatchField, class GeoMesh>
+  void GeometricFieldHolder< Type, PatchField, GeoMesh >::operator-= ( const GeometricFieldHolder< Type, PatchField, GeoMesh >& field)
+  {
+    this->ref() -= field();
+  }
+
+
+} //Foam
+
+
+//---------------------------------------------------------------------------
+
