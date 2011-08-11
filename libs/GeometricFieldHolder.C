623fd2f440646469179429d5608d260abcfdeaca
Andrey Simurzin <asimurzin@gmail.com>
2011-08-11 07:30:27 +0400

    to operator = ( const TypeHolder& ) and operator = ( const Type& )

--- libs/GeometricFieldHolder.C
+++ libs/GeometricFieldHolder.C
@@ -9,9 +9,10 @@ namespace Foam
 {
 
   template<class Type, template<class> class PatchField, class GeoMesh>
-  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder( const IOobjectHolder& io, const fvMeshHolder& mesh ) :
-    GeometricFieldArgs( mesh ),
-    tmp< GeometricField< Type, PatchField, GeoMesh > >( new GeometricField< Type, PatchField, GeoMesh >( *io, *mesh ) )
+  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder( const IOobjectHolder& io, 
+									   const fvMeshHolder& mesh ) 
+    : GeometricFieldArgs( mesh )
+    , tmp< GeometricField< Type, PatchField, GeoMesh > >( new GeometricField< Type, PatchField, GeoMesh >( *io, *mesh ) )
   {
 #ifdef OUR_DEBUG
     cout << "GeometricFieldHolder=" << this << nl;
@@ -19,9 +20,10 @@ namespace Foam
   }
 
   template<class Type, template<class> class PatchField, class GeoMesh>
-  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder( const tmp< GeometricField< Type, PatchField, GeoMesh > >& tmp_gf, const fvMeshHolder& mesh ) : 
-    GeometricFieldArgs( mesh ), 
-    tmp< GeometricField< Type, PatchField, GeoMesh > >( tmp_gf )
+  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder( const tmp< GeometricField< Type, PatchField, GeoMesh > >& tmp_gf, 
+									   const fvMeshHolder& mesh ) 
+    : GeometricFieldArgs( mesh )
+    , tmp< GeometricField< Type, PatchField, GeoMesh > >( tmp_gf )
   {
 #ifdef OUR_DEBUG
     cout << "GeometricFieldHolder=" << this << nl;
@@ -29,14 +31,26 @@ namespace Foam
   }
 
   template<class Type, template<class> class PatchField, class GeoMesh>
-  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder( const IOobjectHolder& io, const GeometricFieldHolder< Type, PatchField, GeoMesh >& field ):
-    GeometricFieldArgs( field.mesh() ),
-    tmp< GeometricField< Type, PatchField, GeoMesh > >( new GeometricField< Type, PatchField, GeoMesh >( *io, field() ) )
+  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder( const IOobjectHolder& io, 
+									   const GeometricFieldHolder< Type, PatchField, GeoMesh >& field )
+    : GeometricFieldArgs( field.mesh() )
+    , tmp< GeometricField< Type, PatchField, GeoMesh > >( new GeometricField< Type, PatchField, GeoMesh >( *io, field() ) )
   {
 #ifdef OUR_DEBUG
-    cout << "flusurfaceScalarField = " << this << nl;
+    cout << "GeometricFieldHolder=" << this << nl;
 #endif
   }
+
+  template<class Type, template<class> class PatchField, class GeoMesh>
+  GeometricFieldHolder< Type, PatchField, GeoMesh >::GeometricFieldHolder()
+    : GeometricFieldArgs()
+    , tmp< GeometricField< Type, PatchField, GeoMesh > >()
+  {
+#ifdef OUR_DEBUG
+    cout << "GeometricFieldHolder=" << this << nl;
+#endif
+  }
+
 
   template<class Type, template<class> class PatchField, class GeoMesh>
   GeometricFieldHolder< Type, PatchField, GeoMesh >::~GeometricFieldHolder()
@@ -50,24 +64,22 @@ namespace Foam
   //-------------------------------------------------------------------------
   template<class Type, template<class> class PatchField, class GeoMesh>
   GeometricField< Type, PatchField, GeoMesh >& 
-    GeometricFieldHolder< Type, PatchField, GeoMesh >::ref()
+  GeometricFieldHolder< Type, PatchField, GeoMesh >::ref()
   {
     return this->operator()();
   }
   
   template<class Type, template<class> class PatchField, class GeoMesh>
-  void GeometricFieldHolder< Type, PatchField, GeoMesh >::operator = ( const GeometricFieldHolder< Type, PatchField, GeoMesh >& field)
+  void GeometricFieldHolder< Type, PatchField, GeoMesh >::operator = ( const GeometricField< Type, PatchField, GeoMesh >& field )
   {
-    this->ref() = field();
+    this->ref() = field;
   }
   
   template<class Type, template<class> class PatchField, class GeoMesh>
-  void GeometricFieldHolder< Type, PatchField, GeoMesh >::operator-= ( const GeometricFieldHolder< Type, PatchField, GeoMesh >& field)
+  void GeometricFieldHolder< Type, PatchField, GeoMesh >::operator-= ( const GeometricField< Type, PatchField, GeoMesh >& field)
   {
-    this->ref() -= field();
+    this->ref() -= field;
   }
-
-
 } //Foam
 
 
