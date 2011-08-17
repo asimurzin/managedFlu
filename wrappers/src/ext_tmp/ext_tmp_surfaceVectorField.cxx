a391c7799cd772a3f00ff4c985b6c2c8b566c364
Andrey Simurzin <asimurzin@gmail.com>
2011-08-17 13:14:54 +0400

    to wrap ext_tmp fields

---
+++ wrappers/src/ext_tmp/ext_tmp_surfaceVectorField.cxx
@@ -0,0 +1,56 @@
+//  pythonFlu - Python wrapping for OpenFOAM C++ API
+//  Copyright (C) 2010- Alexey Petrov
+//  Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+//  See http://sourceforge.net/projects/pythonflu
+//
+//  Author : Alexey PETROV
+
+
+//---------------------------------------------------------------------------
+#ifndef ext_tmp_surfaceVectorField_cxx
+#define ext_tmp_surfaceVectorField_cxx
+
+
+//---------------------------------------------------------------------------
+%module "wrappers.src.ext_tmp.ext_tmp_surfaceVectorField";
+%{
+  #include "ext_tmp/ext_tmp_surfaceVectorField.hh"
+%}
+
+
+//---------------------------------------------------------------------------
+//%import "ext_tmp/ext_tmp.hxx"
+
+%import "Foam/src/finiteVolume/fvMesh/fvMeshes.cxx"
+
+
+//---------------------------------------------------------------------------
+%template ( ext_tmp_surfaceVectorField ) Foam::ext_tmp< Foam::surfaceVectorField >;
+
+%extend Foam::ext_tmp< Foam::surfaceVectorField >
+{
+  SMARTPTR_EXTEND_OPERATOR_EQ( Foam::vector );
+}
+
+
+//---------------------------------------------------------------------------
+VECTOR_EXT_TMP_GEOMETRIC_FIELD_TEMPLATE_FUNC( Foam::surfaceVectorField, Foam::fvsPatchField, Foam::surfaceMesh );
+
+
+//---------------------------------------------------------------------------
+#endif
+
