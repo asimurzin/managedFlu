8e3ee46107ab6a28092c2389783fad355a814ae2
Andrey Simurzin <asimurzin@gmail.com>
2011-08-17 08:44:08 +0400

    to wrap findRefCell

---
+++ libs/findRefCell.hpp
@@ -0,0 +1,42 @@
+#ifndef findRefCell_hpp
+#define findRefCell_hpp
+
+
+//---------------------------------------------------------------------------
+#include <findRefCell.H>
+#include "volFields.hpp"
+#include "surfaceFields.hpp"
+
+
+//---------------------------------------------------------------------------
+namespace Foam
+{
+  inline void setRefCell( const volScalarFieldHolder& field,
+    const volScalarFieldHolder& fieldRef,
+    const dictionary& dict,
+    label& refCelli,
+    scalar& refValue,
+    const bool forceReference = false )
+  {
+   setRefCell( field(), fieldRef(), dict, refCelli, refValue, forceReference );
+  }
+  
+  inline void setRefCell( const volScalarFieldHolder& field,
+    const dictionary& dict,
+    label& refCelli,
+    scalar& refValue,
+    const bool forceReference = false )
+  {
+    setRefCell( field, dict, refCelli, refValue, forceReference );
+  }
+  
+  scalar getRefCellValue( const volScalarFieldHolder& field, const label refCelli )
+  {
+    return getRefCellValue( field(), refCelli );
+  }
+
+} // Foam
+
+
+//---------------------------------------------------------------------------
+#endif
