3ec2b9fb392e1472ba70bc4b1b079e3d9736cad6
Andrey Simurzin <asimurzin@gmail.com>
2011-08-18 10:23:24 +0400

    to wrap setRefCell

--- wrappers/finiteVolume/__init__.py
+++ wrappers/finiteVolume/__init__.py
@@ -1,8 +1,10 @@
 from wrappers.src.fvMeshHolder import *
 from wrappers.src.GeometricFieldHolders import *
 from wrappers.src.fvMatrices import *
+from wrappers.src.findRefCell import *
 
 from wrappers.src.linear import *
+from wrappers.src.adjustPhi import *
 
 #----------------------------------------------------------------------------
 volScalarFieldHolder = GeometricFieldHolder_scalar_fvPatchField_volMesh
@@ -10,4 +12,13 @@ volVectorFieldHolder = GeometricFieldHolder_vector_fvPatchField_volMesh
 
 surfaceScalarFieldHolder = GeometricFieldHolder_scalar_fvsPatchField_surfaceMesh
 surfaceVectorFieldHolder = GeometricFieldHolder_vector_fvsPatchField_surfaceMesh
+
+
+#----------------------------------------------------------------------------
+def setRefCell( *args ):
+    from wrappers.finiteVolume import ext_setRefCell
+    tmp = ext_setRefCell( *args )
+    return tmp.m_refCelli, tmp.m_refValue
+
+
 
