a624f05cd955879f303068d0734bd10b4f600fba
Andrey Simurzin <asimurzin@gmail.com>
2011-08-23 08:47:24 +0400

    to add new files

--- libs/turbulenceModels.hpp
+++ libs/turbulenceModels.hpp
@@ -1,6 +1,8 @@
 // ************************************************************************* //
+#include "turbulenceModels/compressible/turbulenceModelArgs.hpp"
 #include "turbulenceModels/compressible/turbulenceModelHolder.hpp"
 
+#include "turbulenceModels/compressible/RASModelArgs.hpp"
 #include "turbulenceModels/compressible/RASModelHolder.hpp"
 
 
