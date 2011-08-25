eb400e77a44858dd124fabd422cd578438c9bb1c
Andrey Simurzin <asimurzin@gmail.com>
2011-08-25 09:08:53 +0400

    to add test for universalArgs doesn't  working yet

---
+++ testUniversalArgs/testUniversalArgs.C
@@ -0,0 +1,81 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2004-2011 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    buoyantSimpleRadiationFoam
+
+Description
+    Steady-state solver for buoyant, turbulent flow of compressible fluids,
+    including radiation, for ventilation and heat-transfer.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "basicPsiThermo.H"
+#include "RASModel.H"
+#include "fixedGradientFvPatchFields.H"
+#include "radiationModel.H"
+#include "simpleControl.H"
+#include "core.hpp"
+//#include "Allfunctions.hpp"
+#include "thermophysicalModels.hpp"
+#include "turbulenceModels.hpp"
+
+
+
+//---------------------------------------------------------------------------
+volScalarFieldHolder createFields( const Foam::word& dict_name, const Foam::argList& args )
+{
+  TimeHolder runTime( dict_name, args);
+  
+  fvMeshHolder mesh( IOobjectHolder( fvMesh::defaultRegion,
+				     runTime->timeName(),
+			             runTime,
+				     IOobject::MUST_READ ) );
+  
+  Info<< "Reading thermophysical properties\n" << endl;
+
+  basicPsiThermoHolder pThermo = basicPsiThermoHolder::New( mesh );
+
+  volScalarFieldHolder rho( volScalarField( ( IOobjectHolder( "rho", 
+                                                              runTime->timeName(),
+                                                              mesh, 
+                                                              IOobject::NO_READ, 
+                                                              IOobject::NO_WRITE ),
+                                               pThermo->rho() ) ), universalArgs( &pThermo ) );
+                                               
+   return rho;
+
+}
+
+int main(int argc, char *argv[])
+{
+  Foam::argList args(argc, argv);
+   
+  volScalarFieldHolder rho = createFields( Foam::Time::controlDictName, args );
+
+  return 0;
+}
+
+
+// ************************************************************************* //
