7ed7acdeae9336c0b2e810a3cf5409d500eb2640
Andrey Simurzin <asimurzin@gmail.com>
2011-08-27 10:53:17 +0400

    to define "union" operation over the deps

--- libs/universalArgs.cpp
+++ libs/universalArgs.cpp
@@ -7,24 +7,44 @@ namespace Foam
 {
   universalArgs::universalArgs( const universalHolder* uh )
   {
-    this->args_.insert(  boost::shared_ptr< universalHolder >( uh->clone() ) );
+    this->deps_.insert(  holderPtr( uh->clone() ) );
   }
   
+  universalArgs::universalArgs( const std::set< holderPtr >& the_deps )
+    : deps_( the_deps )
+  {}
+
   universalArgs::universalArgs( const universalHolder* uh1, const universalHolder* uh2 )
   {
-    this->args_.insert( boost::shared_ptr< universalHolder >( uh1->clone() ) );
-    this->args_.insert( boost::shared_ptr< universalHolder >( uh2->clone() ) );
+    this->deps_.insert( holderPtr( uh1->clone() ) );
+    this->deps_.insert( holderPtr( uh2->clone() ) );
   }
 
   universalArgs::universalArgs( const universalHolder* uh1, const universalHolder* uh2, const universalHolder* uh3 )
   {
-    this->args_.insert( boost::shared_ptr< universalHolder >( uh1->clone() ) );
-    this->args_.insert( boost::shared_ptr< universalHolder >( uh2->clone() ) );
-    this->args_.insert( boost::shared_ptr< universalHolder >( uh3->clone() ) );
+    this->deps_.insert( holderPtr( uh1->clone() ) );
+    this->deps_.insert( holderPtr( uh2->clone() ) );
+    this->deps_.insert( holderPtr( uh3->clone() ) );
   }
 
   universalArgs::universalArgs()
   {}
+  
+  const std::set< holderPtr >& universalArgs::deps() const
+  {
+    return deps_;
+  }
+  
+  std::set< holderPtr > operator&&( const std::set< holderPtr >& deps1, const std::set< holderPtr >& deps2)
+  {
+    std::set< holderPtr > result;
+  
+    set_union( deps1.begin(), deps1.end(), deps2.begin(), deps2.end(),
+              std::insert_iterator<std::set< holderPtr > >(result,result.begin()) );
+
+    return result;
+  }
+  
 }//Foam
 
 
--- libs/universalArgs.hpp
+++ libs/universalArgs.hpp
@@ -10,21 +10,32 @@ namespace Foam
 {
   class universalHolder;
   
+  typedef boost::shared_ptr< universalHolder > holderPtr;
+  
   class universalArgs 
   {
+   
     public:
       universalArgs( const universalHolder* uh );
       universalArgs( const universalHolder*, const universalHolder* );
       universalArgs( const universalHolder*, const universalHolder*, const universalHolder* );
+      
+      universalArgs( const std::set< holderPtr >& the_deps );
+      
+      const std::set< holderPtr >& deps() const;
     protected:
       universalArgs();
 
     private:
-      std::set< boost::shared_ptr< universalHolder > > args_;
-
+      std::set< holderPtr > deps_;
+      
   };
+
+  std::set< holderPtr > operator&&( const std::set< holderPtr >&, const std::set< holderPtr >& );
+    
 } // Foam
 
 
 //---------------------------------------------------------------------------
 #endif
+
