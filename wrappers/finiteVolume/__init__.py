cc22da51b1fee8f72da121df9f383b016304c614
Andrey Simurzin <asimurzin@gmail.com>
2011-09-02 12:12:59 +0400

    to separate common functions

--- wrappers/finiteVolume/__init__.py
+++ wrappers/finiteVolume/__init__.py
@@ -21,4 +21,26 @@ def setRefCell( *args ):
     return tmp.m_refCelli, tmp.m_refValue
 
 
+#----------------------------------------------------------------------------
+def createPhi( runTime, mesh, U ):
+    from Foam.OpenFOAM import ext_Info, nl
+    ext_Info() << "Reading/calculating face flux field phi\n" << nl;
+
+
+    from Foam.OpenFOAM import IOobject, fileName, word
+    from wrappers.OpenFOAM import IOobjectHolder
+    from wrappers import deps
+    from wrappers.finiteVolume import surfaceScalarFieldHolder, surfaceVectorFieldHolder
+    from wrappers.finiteVolume import linearInterpolate
+    phi = surfaceScalarFieldHolder( IOobjectHolder( word( "phi" ),
+                                                    fileName( runTime.timeName() ),
+                                                    mesh,
+                                                    IOobject.READ_IF_PRESENT,
+                                                    IOobject.AUTO_WRITE ),
+                                    linearInterpolate(U) & surfaceVectorFieldHolder( mesh.Sf(), deps( mesh ) ) ) 
+    
+    return phi
+
+
+#--------------------------------------------------------------------------------------
 
