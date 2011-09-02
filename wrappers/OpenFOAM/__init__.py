f35a4ab9d82381aa50aad6484365942b40c43dc5
Andrey Simurzin <asimurzin@gmail.com>
2011-09-02 12:12:40 +0400

    to separate common functions

--- wrappers/OpenFOAM/__init__.py
+++ wrappers/OpenFOAM/__init__.py
@@ -1,3 +1,38 @@
 from wrappers.src.OpenFOAM.TimeHolder import *
 from wrappers.src.OpenFOAM.IOobjectHolder import *
 from wrappers.src.OpenFOAM.IOdictionaryHolder import *
+
+
+#---------------------------------------------------------------------------
+def createTime( args ):
+    from Foam.OpenFOAM import ext_Info, nl
+    ext_Info() << "Create time\n" << nl
+    
+    from Foam.OpenFOAM import Time
+    from wrappers.OpenFOAM import TimeHolder 
+
+    runTime = TimeHolder( Time.controlDictName.fget(), args ) 
+    
+    return runTime
+
+#---------------------------------------------------------------------------
+def createMesh( runTime ):
+    from Foam.OpenFOAM import ext_Info, nl
+    ext_Info() << "Create mesh for time = " << runTime.timeName() << nl << nl 
+
+    from Foam.OpenFOAM import IOobject
+    from Foam.OpenFOAM import fileName
+    from Foam.finiteVolume import fvMesh
+    from wrappers.OpenFOAM import IOobjectHolder
+    from wrappers.finiteVolume import fvMeshHolder
+    
+    
+    mesh = fvMeshHolder( IOobjectHolder( fvMesh.defaultRegion.fget(),           
+                                         fileName( runTime.timeName() ),      
+                                         runTime,
+                                         IOobject.MUST_READ ) )                     
+   
+    return mesh
+
+
+#---------------------------------------------------------------------------
