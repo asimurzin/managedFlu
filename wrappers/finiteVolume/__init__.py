55b900e86873438c1c55304ee6bd099569d3a189
Andrey Simurzin <asimurzin@gmail.com>
2011-09-02 15:05:27 +0400

    to add compressible createPhi

--- wrappers/finiteVolume/__init__.py
+++ wrappers/finiteVolume/__init__.py
@@ -43,4 +43,21 @@ def createPhi( runTime, mesh, U ):
 
 
 #--------------------------------------------------------------------------------------
+def compressibleCreatePhi( runTime, mesh, U, rho ):
+     from Foam.OpenFOAM import ext_Info, nl
+     ext_Info() << "Reading/calculating face flux field phi\n" << nl
+     from Foam.OpenFOAM import IOobject, fileName, word
+     from wrappers.OpenFOAM import IOobjectHolder
+     from wrappers import deps
+     from wrappers.finiteVolume import surfaceScalarFieldHolder, surfaceVectorFieldHolder
+     from wrappers.finiteVolume import linearInterpolate
+
+     phi = surfaceScalarFieldHolder( IOobjectHolder( word( "phi" ),
+                                                    fileName( runTime.timeName() ),
+                                                    mesh,
+                                                    IOobject.READ_IF_PRESENT,
+                                                    IOobject.AUTO_WRITE ),
+                                    linearInterpolate( rho * U ) & surfaceVectorFieldHolder( mesh.Sf(), deps( mesh ) ) ) 
+     
+     return phi
 
