489686f4d2eacd733a49b27ee1f56c7613d25c50
Andrey Simurzin <asimurzin@gmail.com>
2011-09-02 10:54:24 +0400

    to wrap shared_ptrs

---
+++ wrappers/src/shared_ptr/shared_ptr_compressible_RASModel.cxx
@@ -0,0 +1,46 @@
+//  pythonFlu - Python wrapping for OpenFOAM C++ API
+//  Copyright (C) 2010- Alexey Petrov
+//  Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  See http://sourceforge.net/projects/pythonflu
+//
+//  Author : Alexey PETROV
+
+
+//---------------------------------------------------------------------------
+#ifndef shared_ptr_compressible_RASModel_cxx
+#define shared_ptr_compressible_RASModel_cxx
+
+
+//---------------------------------------------------------------------------
+%module "wrappers.src.shared_ptr.shared_ptr_compressible_RASModel"
+%{
+  #include "shared_ptr/shared_ptr_compressible_RASModel.hh"
+%}
+
+%include "Foam/ext/common/shared_ptr.hxx"
+
+%import "Foam/src/OpenFOAM/fields/tmp/autoPtr_compressible_RASModel.cxx"
+
+SHAREDPTR_TYPEMAP( Foam::compressible::RASModel );
+
+%ignore boost::shared_ptr< Foam::compressible::RASModel >::operator->;
+
+%template( shared_ptr_compressible_RASModel ) boost::shared_ptr< Foam::compressible::RASModel >;
+
+
+//---------------------------------------------------------------------------
+#endif
