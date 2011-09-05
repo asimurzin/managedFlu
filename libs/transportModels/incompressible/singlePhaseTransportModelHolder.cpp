159da010ba685579c051c8c0997476aea2bbe514
Andrey Simurzin <asimurzin@gmail.com>
2011-09-05 14:37:04 +0400

    to wrap singlePhaseTransportModel

---
+++ libs/transportModels/incompressible/singlePhaseTransportModelHolder.cpp
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------
+#include "singlePhaseTransportModelHolder.hpp"
+
+
+//---------------------------------------------------------------------------
+namespace Foam
+{
+  
+  singlePhaseTransportModelHolder::singlePhaseTransportModelHolder( const volVectorFieldHolder& U, const surfaceScalarFieldHolder& phi )
+    : DependentHolder( Foam::deps( &U, &phi ) )
+    , boost::shared_ptr< singlePhaseTransportModel >( new singlePhaseTransportModel( U(), phi() )
+  {
+    transportModelHolder::operator=( boost::shared_ptr< singlePhaseTransportModel >( *this ) );
+  }
+    
+  singlePhaseTransportModelHolder::singlePhaseTransportModelHolder() 
+    : transportModelHolder()
+    , boost::shared_ptr< singlePhaseTransportModel >()
+  {}
+
+  void singlePhaseTransportModelHolder::operator=( const boost::shared_ptr< singlePhaseTransportModel >& bt ) 
+  {
+    boost::shared_ptr< singlePhaseTransportModel >::operator=( bt );
+    transportModelHolder::operator=( bt );
+  }
+
+  SimpleHolder* singlePhaseTransportModelHolder::clone() const
+  {
+    return new singlePhaseTransportModelHolder( *this );
+  }
+  
+  singlePhaseTransportModelHolder::~singlePhaseTransportModelHolder()
+  {}
+}
+
+
+//---------------------------------------------------------------------------
---
+++ libs/transportModels/incompressible/singlePhaseTransportModelHolder.hpp
@@ -0,0 +1,39 @@
+#ifndef singlePhaseTransportModelHolder_hpp
+#define singlePhaseTransportModelHolder_hpp
+
+
+//---------------------------------------------------------------------------
+#include "transportModelHolder.hpp"
+#include "volFields.hpp"
+#include "surfaceFields.hpp"
+
+#include <singlePhaseTransportModel.H>
+#include <boost/shared_ptr.hpp>
+
+
+//---------------------------------------------------------------------------
+namespace Foam
+{
+  
+  class singlePhaseTransportModelHolder 
+    : public transportModelHolder
+    , public boost::shared_ptr< singlePhaseTransportModel >
+  {
+  public:
+    singlePhaseTransportModelHolder( const volVectorFieldHolder&, const surfaceScalarFieldHolder& );
+    singlePhaseTransportModelHolder();
+    
+    ~singlePhaseTransportModelHolder();
+    
+    virtual SimpleHolder* clone() const;
+    
+    using  boost::shared_ptr< singlePhaseTransportModel >::operator*;
+    using  boost::shared_ptr< singlePhaseTransportModel >::operator->;
+  protected:
+    void operator=( const boost::shared_ptr< singlePhaseTransportModel >& );
+  };
+} // Foam
+
+
+//---------------------------------------------------------------------------
+#endif
