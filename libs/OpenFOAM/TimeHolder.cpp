b3a2e929235dbdc9a3b287c694917a0cbc3bcf82
Andrey Simurzin <asimurzin@gmail.com>
2011-09-15 12:01:35 +0400

    to propogated idea to cpp files

--- libs/OpenFOAM/TimeHolder.cpp
+++ libs/OpenFOAM/TimeHolder.cpp
@@ -7,14 +7,7 @@
 
 
 //---------------------------------------------------------------------------
-#if FOAM_VERSION( >=, 020000 )
-#include "TimeHolder_020000.cpp"
-#endif
-
-
-#if FOAM_VERSION( <, 020000 )
-#include "TimeHolder_010701.cpp"
-#endif
+#include FILE_NAME(TimeHolder,cpp)
 
 
 //---------------------------------------------------------------------------
