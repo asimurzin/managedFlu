7ca5272891b57d8891e7b31cb7a78b382e6ef965
Andrey Simurzin <asimurzin@gmail.com>
2011-09-15 11:32:05 +0400

    using HEADER macro

--- libs/OpenFOAM/TimeHolder.hpp
+++ libs/OpenFOAM/TimeHolder.hpp
@@ -7,14 +7,7 @@
 
 
 //---------------------------------------------------------------------------
-#if FOAM_VERSION( >=, 020000 )
-#include "TimeHolder_020000.hpp"
-#endif
-
-
-#if FOAM_VERSION( <, 020000 )
-#include "TimeHolder_010701.hpp"
-#endif
+#include HEADER_NAME(TimeHolder)
 
 
 //---------------------------------------------------------------------------
