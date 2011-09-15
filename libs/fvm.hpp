2e63aa2e6f4c1f4fcc1ccb4fbdeb3b36fb158fae
Andrey Simurzin <asimurzin@gmail.com>
2011-09-15 15:45:30 +0400

    it is temporary solution During  wrapping of new solvers all functions will be defined in the own file and will be ported by "include" technique

--- libs/fvm.hpp
+++ libs/fvm.hpp
@@ -3,6 +3,8 @@
 
 
 //---------------------------------------------------------------------------
+#include "common.hpp"
+
 #include <fvm.H>
 #include <dimensionedScalar.H>
 #include "surfaceFields.hpp"
@@ -16,11 +18,11 @@ namespace Foam
   namespace fvm
   {
     //-----------------------------------------------------------------------
+#if FOAM_VERSION( >=, 020000 )    
     inline fvVectorMatrixHolder ddt( const volVectorFieldHolder& field )
     {
       return fvVectorMatrixHolder( ddt( field() ), &field );
     }
-
 
     //-----------------------------------------------------------------------
     inline fvVectorMatrixHolder div( const surfaceScalarFieldHolder& field1, 
@@ -42,6 +44,35 @@ namespace Foam
     {
       return fvScalarMatrixHolder( laplacian( field1(), field2() ), Deps( &field1, &field2 )  );
     }
+#endif
+
+#if FOAM_VERSION( <, 020000 )
+    inline fvVectorMatrixHolder ddt( volVectorFieldHolder& field )
+    {
+      return fvVectorMatrixHolder( ddt( field() ), &field );
+    }
+
+    //-----------------------------------------------------------------------
+    inline fvVectorMatrixHolder div( const surfaceScalarFieldHolder& field1, 
+				     volVectorFieldHolder& field2 )
+    {
+      return fvVectorMatrixHolder( div( field1(), field2() ), Deps( &field1, &field2 ) );
+    }
+    
+
+    //-----------------------------------------------------------------------
+    inline fvVectorMatrixHolder laplacian( const dimensionedScalar& ds, 
+                                           volVectorFieldHolder& field )
+    {
+      return fvVectorMatrixHolder( laplacian( ds, field() ), &field );
+    }
+
+    inline fvScalarMatrixHolder laplacian( const volScalarFieldHolder& field1, 
+					   volScalarFieldHolder& field2 )
+    {
+      return fvScalarMatrixHolder( laplacian( field1(), field2() ), Deps( &field1, &field2 )  );
+    }
+#endif
   } // fvm
 } // Foam
 
