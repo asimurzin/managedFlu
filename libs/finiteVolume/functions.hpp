7f9487e83644e34c9a4c9dc43a07d7020b6cf6b7
Andrey Simurzin <asimurzin@gmail.com>
2011-10-05 10:30:24 +0400

    to separate different functions( begining )

--- libs/finiteVolume/functions.hpp
+++ libs/finiteVolume/functions.hpp
@@ -90,18 +90,4 @@ surfaceScalarFieldHolder createPhi(
 
 
 //---------------------------------------------------------------------------
-surfaceScalarFieldHolder compressibleCreatePhi( 
-  const TimeHolder& runTime, 
-  const fvMeshHolder& mesh, 
-  const volVectorFieldHolder& U,
-  const volScalarFieldHolder& rho )
-{
-  Info<< "Reading/calculating face flux field phi\n" << endl;
-  
-  return surfaceScalarFieldHolder( IOobjectHolder( "phi",
-                                                   runTime->timeName(),
-                                                   mesh,
-                                                   IOobject::READ_IF_PRESENT,
-                                                   IOobject::AUTO_WRITE ),
-                                    linearInterpolate( rho * U ) & surfaceVectorFieldHolder( mesh->Sf(), &mesh ) );
-}
+
