3af1072dc1b071bad12b7163c9aae69de166da0d
Andrey Simurzin <asimurzin@gmail.com>
2011-10-26 16:34:37 +0400

    to add static function New

--- libs/turbulenceModels/incompressible/turbulenceModel/turbulenceModelHolder.hpp
+++ libs/turbulenceModels/incompressible/turbulenceModel/turbulenceModelHolder.hpp
@@ -1,3 +1,26 @@
+//  managedFlu - OpenFOAM C++ interactive functionality API
+//  Copyright (C) 2010- Alexey Petrov
+//  Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  See http://sourceforge.net/projects/pythonflu
+//
+//  Author : Alexey PETROV, Andrey Simurzin
+
+
+//---------------------------------------------------------------------------
 #ifndef turbulenceModelHolder_hpp
 #define turbulenceModelHolder_hpp
 
@@ -7,6 +30,7 @@
 #include "Deps.hpp"
 #include "volFields.hpp"
 #include "surfaceFields.hpp"
+#include "transportModelHolder.hpp"
 
 #include <incompressible/turbulenceModel/turbulenceModel.H>
 #include <boost/shared_ptr.hpp>
@@ -24,9 +48,24 @@ namespace incompressible
   {
   public:
     turbulenceModelHolder();
+
+    turbulenceModelHolder( 
+      const boost::shared_ptr< turbulenceModel >&, 
+      const volVectorFieldHolder&,
+      const surfaceScalarFieldHolder&,
+      transportModelHolder& );
+
+    static turbulenceModelHolder New( 
+      const volVectorFieldHolder& U,
+      const surfaceScalarFieldHolder& phi,
+      transportModelHolder& transport,
+      const word& turbulenceModelName = turbulenceModel::typeName );
+
     ~turbulenceModelHolder();
     
     virtual SimpleHolder* clone() const;
+    
+    void operator()( const turbulenceModelHolder& );
     
     using  boost::shared_ptr< turbulenceModel >::operator*;
     using  boost::shared_ptr< turbulenceModel >::operator->;
