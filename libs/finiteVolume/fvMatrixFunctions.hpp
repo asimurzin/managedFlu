5e524056a1b8add5d0cccb51caf4e11e65b361fd
Andrey Simurzin <asimurzin@gmail.com>
2011-11-03 08:14:45 +0300

    replace operators with template operators

--- libs/finiteVolume/fvMatrixFunctions.hpp
+++ libs/finiteVolume/fvMatrixFunctions.hpp
@@ -14,61 +14,49 @@
 namespace Foam
 {
   //---------------------------------------------------------------------------
-  // fvVectorMatrix operators
-  inline fvVectorMatrixHolder operator + ( const fvVectorMatrixHolder& mt1, 
-					   const fvVectorMatrixHolder& mt2 )
+  template< class Type >
+  inline fvMatrixHolder< Type > operator + ( const fvMatrixHolder< Type >& mt1, 
+                                             const fvMatrixHolder< Type >& mt2 )
   {
-    return fvVectorMatrixHolder( mt1() + mt2(), Deps( mt1.deps(), mt2.deps() ) );
+    return fvMatrixHolder< Type >( mt1() + mt2(), Deps( mt1.deps(), mt2.deps() ) );
   }
 
-  inline fvVectorMatrixHolder operator - ( const fvVectorMatrixHolder& mt1, 
-					   const fvVectorMatrixHolder& mt2 )
+  template< class Type >
+  inline fvMatrixHolder< Type > operator - ( const fvMatrixHolder< Type >& mt1, 
+                                             const fvMatrixHolder< Type >& mt2 )
   {
-    tmp< fvVectorMatrix > result = mt1() - mt2();
-
-    return fvVectorMatrixHolder( mt1() - mt2(), Deps( mt1.deps(), mt2.deps() ) );
+    return fvMatrixHolder< Type >( mt1() - mt2(), Deps( mt1.deps(), mt2.deps() ) );
   }
 
-  inline fvVectorMatrixHolder operator + ( const fvVectorMatrixHolder& mt1, 
-					    const volVectorFieldHolder& field )
+  template< class Type >
+  inline fvMatrixHolder< Type > operator + ( const fvMatrixHolder< Type >& mt1, 
+                                             const GeometricFieldHolder< Type, fvPatchField, volMesh >& field )
   {
-    return fvVectorMatrixHolder( mt1() + field() , Deps( mt1.deps(), &field ) );
+    return fvMatrixHolder< Type >( mt1() + field() , Deps( mt1.deps(), &field ) );
+  }
+  
+  template< class Type >
+  inline fvMatrixHolder< Type > operator - ( const fvMatrixHolder< Type >& mt1, 
+                                             const GeometricFieldHolder< Type, fvPatchField, volMesh >& field )
+  {
+    return fvMatrixHolder< Type >( mt1() - field() , Deps( mt1.deps(), &field ) );
   }
 
-  inline fvVectorMatrixHolder operator == ( const fvVectorMatrixHolder& mt1, 
-					    const fvVectorMatrixHolder& mt2 )
+  template< class Type >
+  inline fvMatrixHolder< Type > operator == ( const fvMatrixHolder< Type >& mt1, 
+                                              const fvMatrixHolder< Type >& mt2 )
   {
     return fvVectorMatrixHolder( mt1() == mt2(), Deps( mt1.deps(), mt2.deps() ) );
   }
-  
-  inline fvVectorMatrixHolder operator == ( const fvVectorMatrixHolder& mt1, 
-					    const volVectorFieldHolder& field )
-  {
-    return fvVectorMatrixHolder( mt1() == field() , Deps( mt1.deps(), &field ) );
-  }
-  
-  
-  //---------------------------------------------------------------------------
-  // fvScalarMatrix operators
-  inline fvScalarMatrixHolder operator + ( const fvScalarMatrixHolder& mt1, 
-					   const fvScalarMatrixHolder& mt2 )
-  {
-    return fvScalarMatrixHolder( mt1() + mt2(),  Deps( mt1.deps(), mt2.deps() ) );
-  }
 
-  inline fvScalarMatrixHolder operator + ( const fvScalarMatrixHolder& mt1, 
-					    const volScalarFieldHolder& field )
+  template< class Type >
+  inline fvMatrixHolder< Type > operator == ( const fvMatrixHolder< Type >& mt1, 
+                                              const GeometricFieldHolder< Type, fvPatchField, volMesh >& field )
   {
-    return fvScalarMatrixHolder( mt1() + field() , Deps( mt1.deps(), &field ) );
+    return fvMatrixHolder< Type >( mt1() == field() , Deps( mt1.deps(), &field ) );
   }
   
-  inline fvScalarMatrixHolder operator == ( const fvScalarMatrixHolder& mt1, 
-					    const volScalarFieldHolder& field )
-  {
-    return fvScalarMatrixHolder( mt1() == field() , Deps( mt1.deps(), &field ) );
-  }
-
-
+  
   //---------------------------------------------------------------------------
 } // Foam
 
